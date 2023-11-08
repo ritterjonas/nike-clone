7b451c69d4238cba144db1cc587ce508859de10b
Jonas Ritter <jonas.ritter@mercadolivre.com>
2023-11-08 17:40:43 -0300

    feat: create rewrites and redirects

---
+++ config/routes.js
@@ -0,0 +1,16 @@
+const rewritesRules = [
+    {
+        source: '/lojas',
+        destination: '/stores',
+    },
+];
+
+const redirectRules = [
+    {
+        source: '/',
+        destination: '/lojas',
+        permanent: true,
+    },
+];
+
+module.exports = { rewritesRules, redirectRules };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const { redirectRules, rewritesRules } = require('./config/routes');
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return rewritesRules;
+  },
+  async redirects() {
+    return redirectRules;
+  },
 }
 
 module.exports = nextConfig
