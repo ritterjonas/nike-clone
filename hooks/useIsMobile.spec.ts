b40f4379dab02cd8da0061c4c4f583ebeed0fa6b
Jonas Ritter <jonas.ritter@mercadolivre.com>
2023-11-09 18:00:44 -0300

    feat: useIsMobile hook

---
+++ hooks/useIsMobile.spec.ts
@@ -0,0 +1,31 @@
+import { renderHook, act } from '@testing-library/react';
+import { useIsMobile } from './useIsMobile';
+
+describe('useIsMobile hook', () => {
+  it('should initialize as false by default', () => {
+    const { result } = renderHook(() => useIsMobile());
+    expect(result.current).toBe(false);
+  });
+
+  it('should update to true when window width is less than 1024 pixels', () => {
+    const { result } = renderHook(() => useIsMobile());
+
+    act(() => {
+      global.innerWidth = 800;
+      global.dispatchEvent(new Event('resize'));
+    });
+
+    expect(result.current).toBe(true);
+  });
+
+  it('should update to false when window width is greater than or equal to 1024 pixels', () => {
+    const { result } = renderHook(() => useIsMobile());
+
+    act(() => {
+      global.innerWidth = 1200;
+      global.dispatchEvent(new Event('resize'));
+    });
+
+    expect(result.current).toBe(false);
+  });
+});
---
+++ hooks/useIsMobile.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth < 1024) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return isMobile;
+};
